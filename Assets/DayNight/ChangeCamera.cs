8d23fb6a449a50b40e1ac2a8604c8ab9a0d054f0
이현수 <heonsuu159@naver.com>
2022-05-25 03:22:20 +0900

    [Lee] mouse_look 제거 , 자유시점에서 shift , q , e (가속 위 아래) 추가 , z를 눌러서 1인칭 3인칭 변경

--- Assets/DayNight/ChangeCamera.cs
+++ Assets/DayNight/ChangeCamera.cs
@@ -4,24 +4,59 @@ using UnityEngine;
 
 public class ChangeCamera : MonoBehaviour
 {
-    public Camera FreeCamera;
-    public Camera PersonCamera;
+    public GameObject FreeCamera;
+    public GameObject PersonCamera;
+    public Transform PersonCamera1;
+    //public Transform FreeCamera1;
+    //public Transform PersonCamera1;
+   //public Transform PersonCamera2;
 
- 
+    public bool change;
+
     // Start is called before the first frame update
     void Start()
     {
-        FreeCamera.enabled = false;
-        PersonCamera.enabled = true;
+        change = false;
+        FreeCamera.SetActive(false);
+        PersonCamera.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.K))
+        
+        if (Input.GetKeyDown(KeyCode.Z))
         {
-            PersonCamera.enabled = false;
-            FreeCamera.enabled = true;
+            if (change == false)
+            {
+                change = true;
+                FreeCameraChange();
+            }
+            else
+            {
+                change = false;
+                PersonCameraChange();
+            }
         }
+      
+    }
+
+    void FreeCameraChange()
+    {
+        
+        FreeCamera.transform.position = PersonCamera.transform.position + new Vector3(0,5,0);
+        FreeCamera.transform.localRotation = PersonCamera1.rotation;
+
+        FreeCamera.SetActive(true);
+        PersonCamera.SetActive(false);
+    }
+
+    void PersonCameraChange()
+    {
+        
+        PersonCamera.transform.position = FreeCamera.transform.position + new Vector3(0,20,0);
+        
+        FreeCamera.SetActive(false);
+        PersonCamera.SetActive(true);
     }
 }
