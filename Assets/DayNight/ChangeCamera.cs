80cad1c97a19f3a7d2e70177ee4a4cd39671e504
천성원 <chun_sungwon@naver.com>
2022-05-25 16:35:49 +0900

    [chun] 카메라 핫픽스 완료. 카메라 전환 rotation 문제 해결. 메인씬 정리.

--- Assets/DayNight/ChangeCamera.cs
+++ Assets/DayNight/ChangeCamera.cs
@@ -7,15 +7,20 @@ public class ChangeCamera : MonoBehaviour
     public GameObject FreeCamera;
     public GameObject PersonCamera;
     public Transform PersonCamera1;
+
+    private UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController personCameraController;
+    private DigitalRuby.RainMaker.CameraSetting freeCameraController;
     //public Transform FreeCamera1;
     //public Transform PersonCamera1;
-   //public Transform PersonCamera2;
+    //public Transform PersonCamera2;
 
     public bool change;
 
     // Start is called before the first frame update
     void Start()
     {
+        personCameraController = PersonCamera.GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController>();
+        freeCameraController = FreeCamera.GetComponent<DigitalRuby.RainMaker.CameraSetting>();
         change = true;
         FreeCamera.SetActive(true);
         PersonCamera.SetActive(false);
@@ -43,9 +48,13 @@ public class ChangeCamera : MonoBehaviour
 
     void FreeCameraChange()
     {
-        
-        FreeCamera.transform.position = PersonCamera.transform.position + new Vector3(0,5,0);
-        FreeCamera.transform.localRotation = PersonCamera1.rotation;
+        FreeCamera.transform.position = PersonCamera1.transform.position + new Vector3(0,5,0);
+
+        var angleX = PersonCamera1.transform.localRotation.eulerAngles.x;
+        if (angleX >= 300f) angleX -= 360f; //360 대신 0, 350 대신 -10, .... 300 대신 -60. Clamp 때문에.
+        var angleY = PersonCamera.transform.localRotation.eulerAngles.y;
+
+        freeCameraController.Init(angleX,angleY);
 
         FreeCamera.SetActive(true);
         PersonCamera.SetActive(false);
@@ -53,8 +62,12 @@ public class ChangeCamera : MonoBehaviour
 
     void PersonCameraChange()
     {
-        PersonCamera1.rotation = FreeCamera.transform.localRotation;
         PersonCamera.transform.position = FreeCamera.transform.position + new Vector3(0,20,0);
+
+        PersonCamera.transform.localRotation = Quaternion.Euler(0, FreeCamera.transform.localRotation.eulerAngles.y, 0);
+        PersonCamera1.transform.localRotation = Quaternion.Euler(FreeCamera.transform.localRotation.eulerAngles.x, 0, 0); 
+
+        personCameraController.mouseLook.Init(PersonCamera.transform, PersonCamera1.transform);
         
         FreeCamera.SetActive(false);
         PersonCamera.SetActive(true);
